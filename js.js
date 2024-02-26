e6e2795f6ddabc4a17e81f3151c9778e060a9c84
Pavel <73703906+meow-or@users.noreply.github.com>
2024-02-26 08:59:21 +0500

    sort by age

--- js.js
+++ js.js
@@ -1,12 +1,28 @@
-let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
-let petya = { name: "Петя", surname: "Иванов", id: 2 };
-let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
 
 let users = [ vasya, petya, masha ];
 
-let usersMapped = users.map(u => ({
-    fullName: u.name + ' ' + u.surname,
-    id: u.id
-  }));
+function sortByAge(arr) {
+  return arr.sort((a,b) => a.age - b.age)
+}
 
-console.log(usersMapped);
+
+console.log(sortByAge(users));
+
+
+
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+// let users = [ vasya, petya, masha ];
+
+// let usersMapped = users.map(u => ({
+//     fullName: u.name + ' ' + u.surname,
+//     id: u.id
+//   }));
+
+// console.log(usersMapped);
