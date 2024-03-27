900ea2d37132f4de958325f9f10697f644f6407f
Pavel <73703906+meow-or@users.noreply.github.com>
2024-03-27 09:20:09 +0500

    benchmarks

--- js.js
+++ js.js
@@ -1,8 +1,68 @@
-let date = new Date(2016, 1, 28);
+function diffSub(date1, date2) {
+  return date2 - date1;
+}
 
-date.setDate(date.getDate() + 2);
+function diffGetTime(date1, date2) {
+  return date2.getTime() - date1.getTime();
+}
 
-console.log(date);
+function bench(f) {
+  let date1 = new Date(0);
+  let date2 = new Date();
+
+  let start = Date.now();
+
+  for (let i = 0; i < 100000; i++) {
+    f(date1, date2);
+  }
+
+  return Date.now() - start;
+}
+
+let time1 = 0;
+let time2 = 0;
+
+bench(diffSub);
+bench(diffGetTime);
+
+for (let i = 0; i < 10; i++) {
+  time1 += bench(diffSub);
+  time2 += bench(diffGetTime);
+}
+
+console.log(time1, time2)
+
+//console.log(bench(diffSub), bench(diffGetTime));
+
+
+// let start = Date.now();
+
+// for (let i = 0; i < 100000; i++) {
+//   let d = i ** 3;
+  
+// }
+
+// let end = Date.now();
+
+// console.log(end - start);
+
+
+// let start = new Date();
+
+// for (let i = 0; i < 100000; i++) {
+//   let dos = i * i * i * i * i;
+// }
+
+// let end = new Date();
+
+// console.log(end - start)
+
+
+// let date = new Date(2016, 1, 28);
+
+// date.setDate(date.getDate() + 2);
+
+// console.log(date);
 
 // let date = new Date(2013, 0, 34);
 // console.log(date);
