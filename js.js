017ac07189edd6afbe63146eb1921b08926722bc
Pavel <73703906+meow-or@users.noreply.github.com>
2024-04-08 08:59:34 +0500

    JSON space, parse, reviver

--- js.js
+++ js.js
@@ -1,30 +1,88 @@
-let room = {
-  number: 23
-};
+let schedule = `{
+  "meetups": [
+    {"title":"Conference","date":"2017-11-30T12:00:00.000Z"},
+    {"title":"Birthday","date":"2017-04-18T12:00:00.000Z"}
+  ]
+}`;
 
-let meetup = {
-  title: "Conference",
-  participants: [
-                  { name: 'John' },
-                  { name: 'Alice' }
-                ],
-  place: room
-};
+schedule = JSON.parse(schedule, function (key, value) {
+  if(key == 'date') {
+    return new Date(value);
+  }
 
-room.occupiedBy = meetup;
+  return value;
+});
 
-console.log(JSON.stringify(meetup, function replacer(key, value) {
-  console.log(`${key}: ${value}`);
+console.log(schedule)
 
-  return key == 'occupiedBy' ? undefined : value;
-}));
+// let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+
+// let meetup = JSON.parse(str, function (key, value) {
+//   if (key == 'date') return new Date(value);
+//   return value;
+// });
+
+// console.log(meetup);
+
+// let user = '{ "name": "John", "age": 35, "isAdmin": false, "friends": [0,1,2,3] }';
+
+// user = JSON.parse(user);
+
+// console.log(user)
+
+// let numbers = "[0, 1, 2, 3]";
+
+// numbers = JSON.parse(numbers);
+
+// console.log(numbers)
 
 
+// let room = {
+//   number: 23,
+//   toJSON() {
+//     return this.number;
+//   }
+// };
 
+// let meetup = {
+//   title: "Conference",
+//   date: new Date(Date.UTC(2017, 0, 1)),
+//   room
+// }
 
+// console.log( JSON.stringify(meetup, '', 2))
 
+// let user  = {
+//   name: "John",
+//   age: 25,
+//   roles: {
+//     isAdmin: false,
+//     isEditor: true
+//   }
+// };
 
+// console.log(JSON.stringify(user, null, 4));
 
+// let room = {
+//   number: 23
+// };
+
+// let meetup = {
+//   title: "Conference",
+//   participants: [
+//                   { name: 'John' },
+//                   { name: 'Alice' }
+//                 ],
+//   place: room
+// };
+
+// room.occupiedBy = meetup;
+
+// console.log(JSON.stringify(meetup, function replacer(key, value) {
+//   console.log(`${key}: ${value}`);
+
+//   return key == 'occupiedBy' ? undefined : value;
+// }));
 
 
 //   let student = {
