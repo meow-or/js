0ebda31c2ccd33211544ff43a54233dd9dc8bbe0
Pavel <73703906+meow-or@users.noreply.github.com>
2024-04-12 09:02:56 +0500

    recursion

--- js.js
+++ js.js
@@ -1,21 +1,32 @@
-let room = {
-  number: 23
-};
+function pow(x, n) {
+  if(n == 1) {
+    return x;
+  } else {
+    return x * pow(x, n - 1);
+  }
+}
 
-let meetup = {
-  title: "Совещание",
-  occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
-  place: room
-};
-
-// цикличные ссылки
-room.occupiedBy = meetup;
-meetup.self = meetup;
+console.log(pow(2, 10));
 
 
-console.log(JSON.stringify(meetup, function replacer(key, value) {
-    return (key != "" && value == meetup) ? undefined : value;
-}, 2))
+// let room = {
+//   number: 23
+// };
+
+// let meetup = {
+//   title: "Совещание",
+//   occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+//   place: room
+// };
+
+// // цикличные ссылки
+// room.occupiedBy = meetup;
+// meetup.self = meetup;
+
+
+// console.log(JSON.stringify(meetup, function replacer(key, value) {
+//     return (key != "" && value == meetup) ? undefined : value;
+// }, 2))
 
 
 
