194ec25258059cc9e818fbfc6ab9dbcc61645154
Pavel <73703906+meow-or@users.noreply.github.com>
2024-05-27 09:02:02 +0500

    environment

--- js.js
+++ js.js
@@ -1,21 +1,56 @@
-function sumTo1(x) {
-  let sum = 0;
+{
+  let str = 'string';
 
-  for (let i = 0; i <= x; i++) {
-    sum += i;
-  }
-
-  return sum;
+  //console.log(str)
 }
 
-function recSum(x) {
+console.log(str)
 
-  if(x == 1) {
-    return x;
-  } else {
-    return x += recSum(x - 1);
-  }
+// let str = 'hello world';
 
-}
+// console.log([...str])
 
-console.log(recSum(3));
+// function f() {
+//   let showArg = () => {
+//     console.log(arguments[1]);
+//   }
+
+//   showArg(3)
+// }
+
+// f(1, 2, 3);
+
+// function sumAll (a, b, ...args) {
+//   let sum = 0;
+
+//   for (let arg of args) {
+//     sum += arg;
+//   }
+
+//   console.log(args);
+
+//   return a + b;
+// }
+
+// console.log(sumAll(10, 12, 13, 14, 15, 16, 17, 18));
+// function sumTo1(x) {
+//   let sum = 0;
+
+//   for (let i = 0; i <= x; i++) {
+//     sum += i;
+//   }
+
+//   return sum;
+// }
+
+// function recSum(x) {
+
+//   if(x == 1) {
+//     return x;
+//   } else {
+//     return x += recSum(x - 1);
+//   }
+
+// }
+
+// console.log(recSum(3));
