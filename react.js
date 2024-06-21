62cd72a01e1f8b98e97eb10e146a323cbda3ef99
Pavel <73703906+meow-or@users.noreply.github.com>
2024-06-21 09:04:01 +0500

    react render

--- react.js
+++ react.js
@@ -1,79 +1,42 @@
-function UserGreeting(props) {
-  return <h1>C возвращением!</h1>
-}
-
-function GuestGreeting(props) {
-  return <h1>Пожалуйста, зарегистрируйтесь</h1>
-}
-
-function Greeting (props) {
-  const isLoggedIn = props.isLoggedIn;
-
-  if(isLoggedIn) {
-    return <UserGreeting/>;
+function WarningBanner(props) {
+  if(!props.warn) {
+    return null;
   }
 
-  return <GuestGreeting/>
-}
-
-function LoginButton(props) {
   return (
-    <button onClick={props.onClick}>Авторизация</button>
-  )
-}
-
-function LogoutButton(props) {
-  return (
-    <button onClick={props.onClick}>Выход</button>
+    <div className="warning">
+        Warning!
+    </div>
   );
 }
 
-class LoginControl extends React.Component {
-  constructor(props) {
-    super(props);
+class Page extends React.Component {
+    constructor(props) {
+      super(props);
 
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-
-    this.state = { isLoggedIn: false };
-  }
-
-
-  handleLoginClick() {
-    this.setState({ isLoggedIn: true });
-  }
-
-  handleLogoutClick() {
-    this.setState({ isLoggedIn: false});
-  }
-
-  render () {
-    const isLoggedIn = this.state.isLoggedIn;
-    let button;
-
-    if(isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick} />
+      this.state = { showWarning: true };
+      this.handkeToggleClick = this.handkeToggleClick.bind(this);
     }
 
-    return (
-      <div>
-        <Greeting isLoggedIn={isLoggedIn} />
-        {button}
-      </div>
-    );
-  };
+    handkeToggleClick() {
+      this.setState(state => ({
+        showWarning: !state.showWarning
+      }));
+    }
 
-  
+    render() {
+      return (
+        <div>
+          <WarningBanner warn={this.state.showWarning} />
+          <button onClick={this.handkeToggleClick}>
+            {this.state.showWarning ? 'Hide': 'Show'}
+          </button>
+        </div>
+      );
+    }
 }
 
-
-
-
-
-
 ReactDOM.render(
-  <LoginControl />,
+  <Page/>,
   document.querySelector('#root')
-)
+);
